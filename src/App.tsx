776441fe0fc0a10981fb38fc4b7c8396534f1b97
Hacker-C <1507559148@qq.com>
2022-12-04 11:29:47 +0800

    💄 ui: adjust style

--- src/App.tsx
+++ src/App.tsx
@@ -35,6 +35,5 @@ export default function App() {
       <TheFooter></TheFooter>
     </Router>
     </>
-
   )
 }
--- src/pages/error-page.tsx
+++ src/pages/error-page.tsx
@@ -1,11 +1,15 @@
 import { useLocation } from 'react-router-dom'
+import { Icon } from '@iconify/react'
 import Back from '@/components/back'
 
 export default function ErrorPage() {
   const { pathname } = useLocation()
   return (
-    <div text-center mt-20>
-      <h1 text-4xl font-bold>Oooops!</h1>
+    <div text-center mt-20 font-sans>
+      <div flex-center>
+        <Icon icon='iconoir:emoji-sad' width='50'/>
+      </div>
+      <h1 text-3xl font-bold>Oooops!</h1>
       <div text-xl mt-10>Sorry, there is something wrong...</div>
       <p mt-10>
         <i text-gray-500 >{`404 not found: ${pathname}`}</i>
