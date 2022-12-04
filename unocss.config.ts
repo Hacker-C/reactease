0cf7359f022f80623e7c16185fa7520f0ba7bad2
Hacker-C <1507559148@qq.com>
2022-12-04 11:27:52 +0800

    🔧 chore: add shortcuts

--- unocss.config.ts
+++ unocss.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
     presetAttributify(),
     presetUno(),
     presetWebFonts({
-      provider: 'google',
+      provider: 'bunny',
       fonts: {
         sans: 'Roboto',
         mono: ['Fira Code', 'Fira Mono:400,700'],
@@ -24,5 +24,6 @@ export default defineConfig({
   shortcuts: {
     'pos-center': 'relative left-[50%] top-[50%] -translate-x-[50%] -translate-y-[50%]',
     'btn-primary': 'bg-primary rounded mt-5 text-white py-1 px-4',
+    'flex-center': 'flex justify-center items-center'
   }
 })
