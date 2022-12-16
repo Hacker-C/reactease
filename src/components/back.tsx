9d5c10ac1852edceba3de548fe0b80ebea2e318b
Hacker-C <1507559148@qq.com>
2022-12-16 22:59:59 +0800

    🐛 fix: adjust button

--- src/components/back.tsx
+++ src/components/back.tsx
@@ -2,8 +2,10 @@ import { Link } from 'react-router-dom'
 
 export default function Back() {
   return (
-    <button className='btn-primary'>
-      <Link to="/">Back</Link>
-    </button>
+    <Link to="/">
+      <button className='btn-primary'>
+        Back
+      </button>
+    </Link>
   )
 }
