36e92eb9246da364d57962f406652657d0ebe8e6
Hacker-C <1507559148@qq.com>
2022-12-16 22:58:22 +0800

    🔧 chore: update deps

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.ts
 import { resolve } from 'path'
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 import unocss from 'unocss/vite'
 
 export default defineConfig({
